bd1396d4009847abe0c4f7f14da515496ec88fe8
Oscar Gonzalez <oscarrrr12@gmail.com>
2024-09-13 19:49:01 +0200

    Fixed node-cron task

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const dotenv = require('dotenv');
-dotenv.config();
 const cors = require('cors');
 const cron = require('node-cron');
+
+dotenv.config();
+
 const secretRepository = require('./repositories/secret.repository');
 
 const app = express();
@@ -15,12 +17,15 @@ app.use(express.json());
 
 require("./routes")(app);
 
-cron.schedule('*/5 * * * *', async () => {
-    dotenv.config();
-    const deletedSecrets = await secretRepository.deleteExpiredSecrets();
-    console.log(deletedSecrets);
-})
+const deleteExpiredSecretsTask = cron.schedule('*/5 * * * *', async () => {
+    try {
+        const deletedSecrets = await secretRepository.deleteExpiredSecrets();
+    } catch (error) {
+        console.error('Error en la tarea cron:', error);
+    }
+});
 
+deleteExpiredSecretsTask.start();
 
 app.listen(port, () => {
     console.log(` Servidor escuchando en http://localhost:${port}`);
