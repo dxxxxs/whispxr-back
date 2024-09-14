bc65d06f095479cfdea2b77e30b6d1d446aa3c92
Oscar Gonzalez <oscarrrr12@gmail.com>
2024-09-14 02:25:42 +0200

    Fixed controller

--- routes/healthcheck.routes.js
+++ routes/healthcheck.routes.js
@@ -1,5 +1,5 @@
 const controller = require("../controllers/healthcheck.controller");
 
 module.exports = (app) => {
-    app.get('/api/health-check', healtCheckController.healthCheck);
+    app.get('/api/health-check', controller.healthCheck);
 }
